73e90465c3a3e026043ae3504f12ce346eb06513
Brian Lewis <brian@modulesunraveled.com>
2020-01-24 13:40:53 -0600

    Added stories for templates.

---
+++ web/themes/custom/sous/components/04-templates/layouts.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import fullWidthTwig from 'sous-components-twig/04-templates/full-width.twig';
+import withSidebarTwig from 'sous-components-twig/04-templates/with-sidebar.twig';
+
+import mainMenu from 'sous-components-twig/02-molecules/menus/main-menu/main-menu.yml';
+import socialMenu from 'sous-components-twig/02-molecules/menus/social/social-menu.yml';
+import footerMenu from 'sous-components-twig/02-molecules/menus/inline/inline-menu.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Templates/Layouts' };
+
+export const fullWidth = () => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html:
+      fullWidthTwig({ ...mainMenu, ...socialMenu, ...footerMenu }),
+    }}
+  />
+);
+export const withSidebar = () => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html:
+      withSidebarTwig({ ...mainMenu, ...socialMenu, ...footerMenu }),
+    }}
+  />
+);
---
+++ web/themes/custom/sous/components/04-templates/placeholder/place-holder.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import placeHolderTwig from 'sous-components-twig/04-templates/placeholder/place-holder.twig';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Templates/Place Holder' };
+
+export const placeHolder = () => <div dangerouslySetInnerHTML={{ __html: placeHolderTwig({}) }} />;
