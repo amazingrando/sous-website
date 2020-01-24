f355cf61af61f8a2c21480a8fdfc3f8f329f0426
Brian Lewis <brian@modulesunraveled.com>
2020-01-24 14:48:17 -0600

    Created a child card that uses components from both the parent and child theme.

--- web/themes/custom/sous/components/02-molecules/card/cards.stories.js
+++ web/themes/custom/sous/components/02-molecules/card/cards.stories.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+// Parent Components
 import card from 'sous-components-twig/02-molecules/card/card.twig';
-
+// Parent Data
 import cardData from 'sous-components-twig/02-molecules/card/card.yml';
 import cardBgData from 'sous-components-twig/02-molecules/card/card-bg.yml';
+
+// Child Components
+import childCardTwig from './child-card.twig';
+// Child Data
+import childCardData from './child-card.yml';
 
 /**
  * Storybook Definition.
@@ -13,3 +19,4 @@ export default { title: 'Molecules/Cards' };
 export const cardExample = () => <div dangerouslySetInnerHTML={{ __html: card(cardData) }} />;
 export const cardWithBackground = () => (
   <div dangerouslySetInnerHTML={{ __html: card({ ...cardData, ...cardBgData }) }} />);
+export const childCard = () => <div dangerouslySetInnerHTML={{ __html: childCardTwig(childCardData) }} />;
