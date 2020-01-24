7c6d1bf20405db1be8ef8c0a76805233cd368f1e
Brian Lewis <brian@modulesunraveled.com>
2020-01-24 14:35:04 -0600

    Created a custom yml file for child button.

--- web/themes/custom/sous/components/01-atoms/buttons/buttons.stories.js
+++ web/themes/custom/sous/components/01-atoms/buttons/buttons.stories.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import Button from './react/Button.component';
 
+// Parent Components
 import button from 'sous-components-twig/01-atoms/buttons/twig/button.twig';
-import childButtonTwig from './child-button.twig';
-
+// Parent Data
 import buttonData from 'sous-components-twig/01-atoms/buttons/twig/button.yml';
 import buttonAltData from 'sous-components-twig/01-atoms/buttons/twig/button-alt.yml';
 import buttonAlt2Data from 'sous-components-twig/01-atoms/buttons/twig/button-alt2.yml';
+
+// Child Components
+import childButtonTwig from './child-button.twig';
+// Child Data
+import childButtonData from './child-button.yml';
 
 /**
  * Storybook Definition.
@@ -22,4 +27,4 @@ export const react = () => <Button>React Button</Button>;
 export const twig = () => <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />;
 export const twigAlt = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />;
 export const twigAlt2 = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAlt2Data) }} />;
-export const childButton = () => <div dangerouslySetInnerHTML={{ __html: childButtonTwig(buttonData) }} />;
+export const childButton = () => <div dangerouslySetInnerHTML={{ __html: childButtonTwig(childButtonData) }} />;
