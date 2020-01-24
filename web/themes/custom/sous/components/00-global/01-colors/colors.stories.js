4a28633d9a8b01dcdaf4ef7792b0b08be24349fa
Brian Lewis <brian@modulesunraveled.com>
2020-01-24 13:45:03 -0600

    Added stories for global.

---
+++ web/themes/custom/sous/components/00-global/01-colors/colors.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import colors from 'sous-components-twig/00-global/01-colors/colors.twig';
+
+import grayscale from 'sous-components-twig/00-global/01-colors/colors-grayscale.yml';
+import branding from 'sous-components-twig/00-global/01-colors/colors-branding.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Base/Colors' };
+
+export const Grayscale = () => <div dangerouslySetInnerHTML={{ __html: colors(grayscale) }} />;
+export const Branding = () => <div dangerouslySetInnerHTML={{ __html: colors(branding) }} />;
