b5c89a9e6cae3f751dbd8e18ae3e514507164a82
Brian Lewis <brian@modulesunraveled.com>
2020-01-24 13:37:41 -0600

    Added stories for organisms.

---
+++ web/themes/custom/sous/components/03-organisms/grid/grids.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import grid from 'sous-components-twig/03-organisms/grid/grid.twig';
+
+import gridData from 'sous-components-twig/03-organisms/grid/grid.yml';
+import gridCardData from 'sous-components-twig/03-organisms/grid/grid-cards.yml';
+import gridCtaData from 'sous-components-twig/03-organisms/grid/grid-ctas.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Organisms/Grids' };
+
+export const defaultGrid = () => <div dangerouslySetInnerHTML={{ __html: grid(gridData) }} />;
+export const cardGrid = () => (
+  <div dangerouslySetInnerHTML={{ __html: grid({ ...gridData, ...gridCardData }) }} />);
+export const ctaGrid = () => (
+  <div dangerouslySetInnerHTML={{ __html: grid({ ...gridData, ...gridCtaData }) }} />);
---
+++ web/themes/custom/sous/components/03-organisms/site/site.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useEffect } from '@storybook/client-api';
+
+import footerTwig from 'sous-components-twig/03-organisms/site/site-footer/site-footer.twig';
+import siteHeader from 'sous-components-twig/03-organisms/site/site-header/site-header.twig';
+
+import footerSocial from 'sous-components-twig/02-molecules/menus/social/social-menu.yml';
+import footerMenu from 'sous-components-twig/02-molecules/menus/inline/inline-menu.yml';
+import breadcrumbData from 'sous-components-twig/02-molecules/menus/breadcrumbs/breadcrumbs.yml';
+import mainMenubData from 'sous-components-twig/02-molecules/menus/main-menu/main-menu.yml';
+import siteHeaderData from 'sous-components-twig/03-organisms/site/site-header/site-header.yml';
+
+import 'sous-components-twig/02-molecules/menus/main-menu/main-menu';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Organisms/Site' };
+
+export const footer = () => (
+  <div dangerouslySetInnerHTML={{ __html: footerTwig({ ...footerSocial, ...footerMenu }) }} />);
+export const header = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: siteHeader({ ...breadcrumbData, ...mainMenubData, ...siteHeaderData }),
+      }}
+    />
+  );
+};
