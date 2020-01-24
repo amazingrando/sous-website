effe7f4e2a997f781410d7459d9ac220b90988b9
Brian Lewis <brian@modulesunraveled.com>
2020-01-24 14:29:46 -0600

    Created a custom button for the child theme.

--- web/themes/custom/sous/components/01-atoms/buttons/buttons.stories.js
+++ web/themes/custom/sous/components/01-atoms/buttons/buttons.stories.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from './react/Button.component';
 
 import button from 'sous-components-twig/01-atoms/buttons/twig/button.twig';
+import childButtonTwig from './child-button.twig';
 
 import buttonData from 'sous-components-twig/01-atoms/buttons/twig/button.yml';
 import buttonAltData from 'sous-components-twig/01-atoms/buttons/twig/button-alt.yml';
@@ -21,3 +22,4 @@ export const react = () => <Button>React Button</Button>;
 export const twig = () => <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />;
 export const twigAlt = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />;
 export const twigAlt2 = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAlt2Data) }} />;
+export const childButton = () => <div dangerouslySetInnerHTML={{ __html: childButtonTwig(buttonData) }} />;
