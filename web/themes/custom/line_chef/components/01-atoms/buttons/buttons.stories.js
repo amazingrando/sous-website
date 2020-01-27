14d2787073d9a75cd76cd19dbdca99eda9d5832a
Brian Lewis <brian@modulesunraveled.com>
2020-01-27 16:20:28 -0600

    Replaced all references of Parent/Child with Core/Project.

--- web/themes/custom/line_chef/components/01-atoms/buttons/buttons.stories.js
+++ web/themes/custom/line_chef/components/01-atoms/buttons/buttons.stories.js
@@ -9,10 +9,10 @@ import buttonData from 'sous-components-twig/01-atoms/buttons/twig/button.yml';
 import buttonAltData from 'sous-components-twig/01-atoms/buttons/twig/button-alt.yml';
 import buttonAlt2Data from 'sous-components-twig/01-atoms/buttons/twig/button-alt2.yml';
 
-// Child Components
-import childButtonTwig from './child-button.twig';
-// Child Data
-import childButtonData from './child-button.yml';
+// Project Components
+import lcButtonTwig from './lc-button.twig';
+// Project Data
+import lcButtonData from './lc-button.yml';
 
 /**
  * Storybook Definition.
@@ -27,4 +27,4 @@ export const react = () => <Button>React Button</Button>;
 export const twig = () => <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />;
 export const twigAlt = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />;
 export const twigAlt2 = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAlt2Data) }} />;
-export const childButton = () => <div dangerouslySetInnerHTML={{ __html: childButtonTwig(childButtonData) }} />;
+export const lineChefButton = () => <div dangerouslySetInnerHTML={{ __html: lcButtonTwig(lcButtonData) }} />;
--- web/themes/custom/line_chef/components/02-molecules/card/cards.stories.js
+++ web/themes/custom/line_chef/components/02-molecules/card/cards.stories.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
 
 // Parent Components
-import card from 'sous-components-twig/02-molecules/card/card.twig';
+import card from "sous-components-twig/02-molecules/card/card.twig";
 // Parent Data
-import cardData from 'sous-components-twig/02-molecules/card/card.yml';
-import cardBgData from 'sous-components-twig/02-molecules/card/card-bg.yml';
+import cardData from "sous-components-twig/02-molecules/card/card.yml";
+import cardBgData from "sous-components-twig/02-molecules/card/card-bg.yml";
 
-// Child Components
-import childCardTwig from './child-card.twig';
-// Child Data
-import childCardData from './child-card.yml';
+// Project Components
+import lcCardTwig from "./lc-card.twig";
+// Project Data
+import lcCardData from "./lc-card.yml";
 
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/Cards' };
+export default { title: "Molecules/Cards" };
 
-export const cardExample = () => <div dangerouslySetInnerHTML={{ __html: card(cardData) }} />;
+export const cardExample = () => (
+  <div dangerouslySetInnerHTML={{ __html: card(cardData) }} />
+);
 export const cardWithBackground = () => (
-  <div dangerouslySetInnerHTML={{ __html: card({ ...cardData, ...cardBgData }) }} />);
-export const childCard = () => <div dangerouslySetInnerHTML={{ __html: childCardTwig(childCardData) }} />;
+  <div dangerouslySetInnerHTML={{ __html: card({ ...cardData, ...cardBgData }) }} />
+);
+export const lineChefCard = () => (
+  <div dangerouslySetInnerHTML={{ __html: lcCardTwig(lcCardData) }} />
+);
