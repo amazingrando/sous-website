509072b0dd7956c3f2df8c0b09372e12661e1535
Brian Lewis <brian@modulesunraveled.com>
2020-01-27 16:32:11 -0600

    Added story for the project colors.

--- web/themes/custom/line_chef/components/00-base/01-colors/colors.stories.js
+++ web/themes/custom/line_chef/components/00-base/01-colors/colors.stories.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import colors from 'sous-components-twig/00-base/01-colors/colors.twig';
 
+// Core Colors
 import grayscale from 'sous-components-twig/00-base/01-colors/colors-grayscale.yml';
 import branding from 'sous-components-twig/00-base/01-colors/colors-branding.yml';
+// Project Colors
+import lineChef from './colors.yml';
 
 /**
  * Storybook Definition.
@@ -12,3 +15,4 @@ export default { title: 'Base/Colors' };
 
 export const Grayscale = () => <div dangerouslySetInnerHTML={{ __html: colors(grayscale) }} />;
 export const Branding = () => <div dangerouslySetInnerHTML={{ __html: colors(branding) }} />;
+export const LineChef = () => <div dangerouslySetInnerHTML={{ __html: colors(lineChef) }} />;
