0ab3bbd1da5e622fdfd16489e374eecfc7eea3f2
Brian Lewis <brian@modulesunraveled.com>
2020-02-21 06:31:53 -0600

    Fixed colors to merge global and property-specific into one map/function.

--- web/themes/custom/line_chef/components/00-base/01-colors/colors.stories.js
+++ web/themes/custom/line_chef/components/00-base/01-colors/colors.stories.js
@@ -4,6 +4,8 @@ import colors from 'sous-components-twig/00-base/01-colors/colors.twig';
 
 import grayscale from 'sous-components-twig/00-base/01-colors/colors-grayscale.yml';
 import branding from 'sous-components-twig/00-base/01-colors/colors-branding.yml';
+// Line Chef Colors
+import lineChefColors from './lc-colors.yml';
 
 /**
  * Storybook Definition.
@@ -15,4 +17,7 @@ export const Grayscale = () => (
 );
 export const Branding = () => (
   <div dangerouslySetInnerHTML={{ __html: colors(branding) }} />
+);
+export const lineChef = () => (
+  <div dangerouslySetInnerHTML={{ __html: colors(lineChefColors) }} />
 );
